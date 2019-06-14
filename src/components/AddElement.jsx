fd0d9c342777e76c679f5e606c8e54c93d1ecda7
Ashutosh Anand <ashutosh.bsc@gmail.com>
2019-06-14 18:30:09 +0530

    Adding new element in rows

--- src/components/AddElement.jsx
+++ src/components/AddElement.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const AddElement = ({ open, closeAddElement }) => (
+const AddElement = ({ open, closeAddElement, addHeading, addImage }) => (
   <div className={`sidebar add-element ${open ? 'open' : ''}`}>
     <div className="close-btn" onClick={() => closeAddElement()}>X</div>
     <div className="title">Add Element</div>
+    <div className="options">
+      <div className="option" onClick={() => addHeading()}>Heading</div>
+      <div className="option" onClick={() => addImage()}>Image</div>
+    </div>
   </div>
 );
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import Toolbar from './Toolbar';
 import Rows from './Rows';
 import AddRow from './AddRow';
@@ -7,12 +8,15 @@ import Overlay from './Overlay';
 import { HEADING, EMPTY } from '../element_types';
 
 const newId = () => {
-  const date = new Date();
-  return date.getTime();
+  return uuid();
 }
 
 const emptyElement = () => {
   return { type: EMPTY };
+}
+
+const newHeadingElement = () => {
+  return { type: HEADING, data: 'Heading' };
 }
 
 const emptyColumns = (noOfColumns) => {
@@ -26,6 +30,11 @@ const emptyColumns = (noOfColumns) => {
 
   return columns;
 }
+
+const preFilledColumns = (columnElements) => {
+  const columns = columnElements.map(element => ({ id: newId(), element }));
+  return columns;
+};
 
 class Editor extends Component {
   state = {
@@ -52,10 +61,14 @@ class Editor extends Component {
     this.setState({ showAddRow: !showAddRow });
   }
 
+  closeAddRow = () => this.setState({ showAddRow: false })
+
   toggleAddElement = () => {
     const { showAddElement } = this.state;
     this.setState({ showAddElement: !showAddElement });
   }
+
+  closeAddElement = () => this.setState({ showAddElement: false })
 
   overlayClicked = () => {
     const { showAddRow, showAddElement } = this.state;
@@ -69,19 +82,99 @@ class Editor extends Component {
     }
   }
 
-  addRowWithColumns = (columns) => {
-    console.log('add new row with columns --> ', columns);
-    this.toggleAddRow();
+  addRowWithColumns = (noOfColumns) => {
+    console.log('add new row with columns --> ', noOfColumns);
+    this.closeAddRow();
 
+    this.addNewRow({ noOfColumns });
+  }
+
+  addNewRow = ({ noOfColumns, columnElements }) => {
     const newRow = {
       id: newId(),
-      columns: emptyColumns(columns),
+      columns: columnElements ? preFilledColumns(columnElements) : emptyColumns(noOfColumns),
     };
 
     const { rows } = this.state;
     const addingNewRow = [...rows, newRow];
 
     this.setState({ rows: addingNewRow });
+    return newRow.id;
+  }
+
+  getFirstEmptyColumn = () => {
+    const { rows } = this.state;
+    for(let i=0; i<rows.length; i++) {
+      const { id: rowId, columns } = rows[i];
+
+      for(let j=0; j<columns.length; j++) {
+        const { id: columnId, element } = columns[j];
+        if (element.type === EMPTY) {
+          return { row: rowId, column: columnId };
+        }
+      }
+    }
+
+    return { row: null, column: null };
+  }
+
+  addNewElement = ({ element }) => {
+    // Find which row and column to add the heading element
+    let rowId;
+    const { row, column } = this.getFirstEmptyColumn();
+    console.log('got first empty element --> ', row, column);
+
+    if (row === null || column === null) {
+      // new row added
+      const columnElements = [element];
+      this.addNewRow({ noOfColumns: 1, columnElements });
+      return;
+    } else {
+      rowId = row;
+    }
+
+    const { rows } = this.state;
+    const rowPos = rows.findIndex(r => r.id === rowId);
+    if (rowPos === -1) {
+      console.error('couldnt find row --> ', rows, rowId);
+      return;
+    }
+    const rowData = rows[rowPos];
+
+
+    console.log('got row --> ', rowData);
+    const columnPos = column === null ? 0 : rowData.columns.findIndex(c => c.id === column);
+    console.log('got column pos --> ', columnPos);
+    const columnData = rowData.columns[columnPos];
+    console.log('row --> ', rowData, ' column ', columnData);
+
+    const updatedColumnData = {
+      ...columnData,
+      element,
+    };
+
+    const updatedRow = {
+      ...rowData,
+      columns: [...rowData.columns.slice(0, columnPos), updatedColumnData, ...rowData.columns.slice(columnPos + 1)],
+    };
+
+    const updatedRows = [...rows.slice(0, rowPos), updatedRow, ...rows.slice(rowPos + 1)];
+
+    this.setState({ rows: updatedRows });
+  }
+
+  addHeading = () => {
+    console.log('add heading');
+    this.toggleAddElement();
+
+    const newElement = newHeadingElement();
+
+    this.addNewElement({ element: newElement });
+  } 
+
+  addImage = () => {
+    console.log('add image');
+    this.closeAddElement();
   }
 
   render() {
@@ -91,8 +184,13 @@ class Editor extends Component {
       <div className="editor">
         <Toolbar addRowClicked={this.toggleAddRow} addElementClicked={this.toggleAddElement} />
         <Rows rows={rows} />
-        <AddRow open={showAddRow} closeAddRow={() => this.toggleAddRow()} addRowWithColumns={this.addRowWithColumns} />
-        <AddElement open={showAddElement} closeAddElement={() => this.toggleAddElement()} />
+        <AddRow open={showAddRow} closeAddRow={this.toggleAddRow} addRowWithColumns={this.addRowWithColumns} />
+        <AddElement
+          open={showAddElement}
+          closeAddElement={this.toggleAddElement}
+          addHeading={this.addHeading}
+          addImage={this.addImage}
+        />
         <Overlay show={showAddRow || showAddElement} overlayClicked={() => this.overlayClicked()} />
       </div>
     );
